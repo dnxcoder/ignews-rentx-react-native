5a281e21173a5997c9be538fe811d9815d49cb6c
dnxcoder <dnxcoder@gmail.com>
2021-10-24 01:10:34 -0300

    Implemented Accessory component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import Accessory from "../../components/Accessory";
 import BackButton from "../../components/BackButton";
 import ImageSlider from "../../components/ImageSlider";
+
+import speedSvg from "../../assets/speed.svg";
+import accelerationSvg from "../../assets/acceleration.svg";
+import forceSvg from "../../assets/force.svg";
+import gasolineSvg from "../../assets/gasoline.svg";
+import exchangeSvg from "../../assets/exchange.svg";
+import peopleSvg from "../../assets/people.svg";
 
 import {
   Container,
@@ -14,7 +22,8 @@ import {
   Rent,
   Period,
   Price,
-  About
+  About,
+  Accessories,
 } from "./styles";
 
 const CarDetails: React.FC = () => {
@@ -42,6 +51,14 @@ const CarDetails: React.FC = () => {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+        <Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </Accessories>
         <About>
           Este é automóvel desportivo. Surgiu do lendário de lide indultado na
           praça Real Maestrazana de Sevilla. É um belíssimo carro para quem
