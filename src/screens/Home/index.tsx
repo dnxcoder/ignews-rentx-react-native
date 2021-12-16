a7a980b2b42a7f8af6f10e309712bebdf4d91e70
dnxcoder <dnxcoder@gmail.com>
2021-12-15 21:56:02 -0300

    user gestures

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StatusBar, Text } from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { Ionicons } from "@expo/vector-icons";
+import { RectButton, PanGestureHandler } from "react-native-gesture-handler";
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring
+} from "react-native-reanimated";
+
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 
 import Logo from "../../assets/logo.svg";
 import Car from "../../components/Car";
@@ -15,7 +25,6 @@ import {
   TotalCars,
   HeaderContent,
   CarList,
-  MyCarsButton,
 } from "./styles";
 import Load from "../../components/Load";
 import { useTheme } from "styled-components";
@@ -23,6 +32,33 @@ import { useTheme } from "styled-components";
 const Home: React.FC = () => {
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const poisitionY = useSharedValue(0);
+  const poisitionX = useSharedValue(0);
+
+  const myCarsButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: poisitionX.value },
+        { translateY: poisitionY.value },
+      ],
+    };
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any){
+      ctx.positionX = poisitionX.value;
+      ctx.positionY = poisitionY.value;
+    },
+    onActive(event, ctx:any){
+      poisitionX.value = event.translationX + ctx.positionX;
+      poisitionY.value = event.translationY + ctx.positionY;
+    },
+    onEnd(){
+      poisitionX.value = withSpring(0);
+      poisitionY.value = withSpring(0);
+    }
+  });
 
   const navigation: any = useNavigation();
   const theme = useTheme();
@@ -73,11 +109,41 @@ const Home: React.FC = () => {
           )}
         />
       )}
-      <MyCarsButton onPress={handleOpenMyCars}>
-        <Ionicons name="ios-car-sport" size={32} color={theme.colors.shape} />
-      </MyCarsButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            myCarsButtonStyle,
+            {
+              position: "absolute",
+              bottom: 13,
+              right: 22,
+            },
+          ]}
+        >
+          <ButtonAnimated
+            onPress={handleOpenMyCars}
+            style={[styles.button, { backgroundColor: theme.colors.main }]}
+          >
+            <Ionicons
+              name="ios-car-sport"
+              size={32}
+              color={theme.colors.shape}
+            />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
