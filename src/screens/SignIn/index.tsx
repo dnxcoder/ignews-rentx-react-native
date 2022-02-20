b8ac54a2b43535ef2540352d7e1bb1416b64f84d
dnxcoder <dnxcoder@gmail.com>
2022-02-20 02:37:11 -0300

    Validating form

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,16 +4,42 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import PasswordInput from "../../components/PasswordInput";
 import theme from "../../styles/theme";
 import { Container, Header, Title, SubTilte, Footer, Form } from "./styles";
+import * as Yup from "yup";
 
 const SignIn: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  async function handleSignIn() {
+
+    console.log('hellow')
+    try {
+      const schema = Yup.object({
+        email: Yup.string().required("E-mail obrigatório").email('Digite um e-mail válido'),
+        password: Yup.string().required("A senha é obrigatória"),
+      });
+      await schema.validate({ email, password });
+      Alert.alert("tudo certo");
+
+      //Fazer login
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert("Opa", error.message);
+      } else {
+        Alert.alert(
+          "Erro na autenticação",
+          "Ocorreu um erro ao fazer login, verifique as credenciais"
+        );
+      }
+    }
+  }
 
   return (
     <TouchableWithoutFeedback
@@ -60,14 +86,16 @@ const SignIn: React.FC = () => {
           <Footer>
             <Button
               title="Login"
-              onPress={() => {}}
-              enabled={false}
+              onPress={handleSignIn}
+              enabled={true}
               loading={false}
             ></Button>
             <Button
               color={theme.colors.background_seconday}
               title="Criar conta gratuita"
-              onPress={() => {}}
+              onPress={() => {
+                handleSignIn;
+              }}
               enabled={true}
               loading={false}
               light
