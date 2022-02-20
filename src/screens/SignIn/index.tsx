e9780806c507b547f29b6b9991822dc6ad1d7fc2
dnxcoder <dnxcoder@gmail.com>
2022-02-20 00:05:59 -0300

    Accessibility form

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, StatusBar } from "react-native";
+import {
+  View,
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import PasswordInput from "../../components/PasswordInput";
@@ -8,50 +14,62 @@ import { Container, Header, Title, SubTilte, Footer, Form } from "./styles";
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Header />
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <KeyboardAvoidingView behavior="position" enabled>
+        <Container>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <Header />
 
-      <Title>
-        Estamos{"\n"}
-        quase lá
-      </Title>
-      <SubTilte>
-        Faça seu login para{"\n"}
-        começar uma experiência incrível.
-      </SubTilte>
-      <Form>
-        <Input
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <PasswordInput iconName="lock" placeholder="Senha" secureTextEntry />
-        {/* <Input /> */}
-      </Form>
-      <Footer>
-        <Button
-          title="Login"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        ></Button>
-        <Button
-          color={theme.colors.background_seconday}
-          title="Criar conta gratuita"
-          onPress={() => {}}
-          enabled={true}
-          loading={false}
-          light
-        ></Button>
-      </Footer>
-    </Container>
+          <Title>
+            Estamos{"\n"}
+            quase lá
+          </Title>
+          <SubTilte>
+            Faça seu login para{"\n"}
+            começar uma experiência incrível.
+          </SubTilte>
+          <Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              secureTextEntry
+            />
+            {/* <Input /> */}
+          </Form>
+          <Footer>
+            <Button
+              title="Login"
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            ></Button>
+            <Button
+              color={theme.colors.background_seconday}
+              title="Criar conta gratuita"
+              onPress={() => {}}
+              enabled={true}
+              loading={false}
+              light
+            ></Button>
+          </Footer>
+        </Container>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
